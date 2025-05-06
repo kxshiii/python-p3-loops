3065e224eda13bd6d98852f81680c08b5a242bdd
kxshiii <brian.gikaru@student.moringaschool.com>
2025-05-06 17:42:20 +0300

    comm

--- lib/looping.py
+++ lib/looping.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    """Prints a countdown from 10 to 1 and then 'Happy New Year!'."""
+
     i = 10
     while i > 0:
         print(i)
@@ -10,12 +10,12 @@ def happy_new_year():
 
 
 def square_integers(int_list):
-    """Takes a list of integers and returns a list of their squares."""
+
     return [x ** 2 for x in int_list]
 
 
 def fizzbuzz():
-    """Prints numbers from 1 to 100 with FizzBuzz rules."""
+
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz")
